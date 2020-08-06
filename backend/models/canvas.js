2f81c9ffe71cac6d9e875144da5ae5fb89669287
Ishdril <berni2015@gmail.com>
2020-08-06 16:07:19 +0200

    fix(canvas model): fixed the dates to generate proper Dates

--- backend/models/canvas.js
+++ backend/models/canvas.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const CanvasSchema = new mongoose.Schema({
-  dateCreated: { type: Date, default: Date.now() },
-  dateModified: { type: Date, default: Date.now() },
+  dateCreated: { type: Date, default: new Date() },
+  dateModified: { type: Date, default: new Date() },
   canvasData: { type: Object, default: null },
   isMainCanvas: { type: Boolean, default: false }, // todo put constraint so only one main canvas
 });
