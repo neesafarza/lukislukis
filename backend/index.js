3e93cd13529e0be3963130f1d7c78591b8006a18
Ishdril <berni2015@gmail.com>
2020-08-06 15:33:09 +0200

    feat(index.js): installed dotenv & generated env files. Uninstalled bodyparser and replaced the functions

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const router = require('./router');
-// TODO: dotenv should be used
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.SERVER_PORT || 8080;
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '500MB', type: 'application/json' }));
-app.use(bodyParser.urlencoded({ limit: '500MB', extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '500MB', type: 'application/json' }));
+app.use(express.urlencoded({ limit: '500MB', extended: true }));
 app.use(router);
 
 mongoose
-  .connect('mongodb://localhost:27017/lukislukis', {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
