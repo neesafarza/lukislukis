f5d6497b72391050b78767fa1fd2380754fc6fc2
Ishdril <berni2015@gmail.com>
2020-08-06 16:02:55 +0200

    fix(canvas model): refactor to use default values. No need for createCanvas in canvasService

--- backend/models/canvas.js
+++ backend/models/canvas.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const CanvasSchema = new mongoose.Schema({
-  dateCreated: Date,
-  dateModified: Date,
-  canvasData: Object,
-  isMainCanvas: Boolean, // todo put constraint so only one main canvas
+  dateCreated: { type: Date, default: Date.now() },
+  dateModified: { type: Date, default: Date.now() },
+  canvasData: { type: Object, default: null },
+  isMainCanvas: { type: Boolean, default: false }, // todo put constraint so only one main canvas
 });
 
 module.exports = mongoose.model('Canvas', CanvasSchema);
