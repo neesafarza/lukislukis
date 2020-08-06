2a2618008a0d1fab8f27c77ef4ced1dfdfd0efff
Ishdril <berni2015@gmail.com>
2020-08-06 17:08:14 +0200

    feat(index.js):global 404 error and installed helmet for security

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
+
 const router = require('./routes/router');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 8080;
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json({ limit: '500MB', type: 'application/json' }));
 app.use(express.urlencoded({ limit: '500MB', extended: true }));
 app.use(router);
+app.all('*', (req, res) => {
+  res.status(404);
+  res.send('<h3>Error 404: page not found</h3>');
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
